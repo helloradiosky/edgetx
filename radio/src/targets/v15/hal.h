b15d458b0f57d7ec390a72b3e80884629d4ca83e
radiosky <839034809@qq.com>
2025-12-07 17:25:23 +0800

    V15 TOUCH OK

--- radio/src/targets/v15/hal.h
+++ radio/src/targets/v15/hal.h
@@ -462,11 +462,11 @@ USART6: EXTMODULE_USART
 #define I2C_B1_GPIO_AF                  LL_GPIO_AF_4
 #define I2C_B1_CLK_RATE                 400000
 
-#define I2C_B2                          I2C4
-#define I2C_B2_SDA_GPIO                 GPIO_PIN(GPIOD, 13) //TP
-#define I2C_B2_SCL_GPIO                 GPIO_PIN(GPIOD, 12)
-#define I2C_B2_GPIO_AF                  LL_GPIO_AF_4
-#define I2C_B2_CLK_RATE                 400000
+#define I2C_B4                          I2C4
+#define I2C_B4_SDA_GPIO                 GPIO_PIN(GPIOD, 13) //TP
+#define I2C_B4_SCL_GPIO                 GPIO_PIN(GPIOD, 12)
+#define I2C_B4_GPIO_AF                  LL_GPIO_AF_4
+#define I2C_B4_CLK_RATE                 400000
 
 // Touch
 #define TOUCH_PANEL_INVERTED
